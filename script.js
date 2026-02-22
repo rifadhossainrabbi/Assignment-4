d176955010b47b624ebf34b4b6aa9453714deff7
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-02-22 07:56:39 +0600

    Add script , Initial Interview card andhover effect

---
+++ script.js
@@ -0,0 +1,6 @@
+// console.log("Clicked");
+
+// Total Card Number Count
+let totalCardNumber = document.getElementById("total_count");
+let allChildNumber = document.querySelector(".all_card_section");
+totalCardNumber.innerText = allChildNumber.children.length;
