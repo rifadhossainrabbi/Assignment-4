414e4334323e4e7712b2f1ae1c9f88ba1a0562f8
rifadhossainrabbi <hossainmrrifad@gmail.com>
2026-02-22 09:33:25 +0600

    Click any button and design  Click button and hide other card and show only relative card

--- script.js
+++ script.js
@@ -4,3 +4,60 @@
 let totalCardNumber = document.getElementById("total_count");
 let allChildNumber = document.querySelector(".all_card_section");
 totalCardNumber.innerText = allChildNumber.children.length;
+
+// Count Available Jobs
+let jobNumber = document.getElementById("job_count");
+jobNumber.innerText = allChildNumber.children.length;
+
+// Click any button and design 
+/**
+ * 1-Get all Items
+ * 2-bg white adding all button
+ * 3-remove all bg blue
+ * 4-design only selected button(add and remove)
+ */
+
+
+function designOnly(id) {
+  // button effect after click
+  // Get the buttons
+  const allButton = document.getElementById("all_btn");
+  const interviewButton = document.getElementById("interview_btn");
+  const rejectButton = document.getElementById("reject_btn");
+  
+  // bg white adding all button
+  allButton.classList.add('bg-white', 'text-gray-500');
+  interviewButton.classList.add('bg-white', 'text-gray-500');
+  rejectButton.classList.add('bg-white', 'text-gray-500');
+  
+  // Remove
+  allButton.classList.remove('bg-blue-500', 'text-white');
+  interviewButton.classList.remove('bg-blue-500', 'text-white');
+  rejectButton.classList.remove('bg-blue-500', 'text-white');
+  
+  // design only selected button
+  const selectButton = document.getElementById(id);
+  
+  selectButton.classList.add('bg-blue-500', 'text-white');
+  selectButton.classList.remove("bg-white", "text-gray-500");
+}
+
+
+// Click button and hide other card and show only relative card
+/**
+ * 1- Get all Card
+ * 2- Hide all Card
+ * 3- Remove hide form selected Card 
+ */
+function showOnly(id2) {
+  const allCardSection = document.getElementById("all_card_sections");
+  const initialCardSection = document.getElementById("initial_interview_card");
+
+  //1-All hide
+  allCardSection.classList.add("hidden");
+  initialCardSection.classList.add("hidden");
+
+  // 2- hide remove selected id2
+  const selectId2 = document.getElementById(id2);
+  selectId2.classList.remove("hidden");
+}
